6027cf7ecffb1036b6ca1f5da08dc916b4e2d3ad
Deep周 <okaydeep@hotmail.com>
2017-05-12 19:15:10 +0800

    更新
    1.刪除不必要的檔案及資料夾
    2.更新其餘Canvas

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -2,6 +2,20 @@
 using System.Collections;
 
 public class CanvasManager : MonoBehaviour {
+	/*
+	 * 0.讀取畫面
+	 * 1.主選單
+	 * 2.遊戲選單
+	 * 3.隊伍資訊
+	 * 4.關卡
+	 * 5.市場
+	 * 6.商店
+	 */
+	public GameObject[] CanvasObjs;
+
+	void Awake () {
+		
+	}
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/GlobalDefine.cs
@@ -0,0 +1,12 @@
+﻿
+public class GlobalDefine {
+	public static string Canvas_LoadingImage = "LoadingImage";
+	public static string Canvas_HomeMenu = "HomeMenu";
+	public static string Canvas_BattleMenu = "BattleMenu";
+	public static string Canvas_TeamInfo = "TeamInfo";
+	public static string Canvas_NormalStage = "NormalStage";
+	public static string Canvas_Market = "Market";
+	public static string Canvas_Setting = "Setting";
+
+
+}
