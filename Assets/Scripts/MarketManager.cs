c7aa9a20bce10895f798bc9450d43fb86aa63287
SlowMoCross <eric030867@hotmail.com>
2017-05-30 22:55:55 +0800

    alter shopstore

--- Assets/Scripts/MarketManager.cs
+++ Assets/Scripts/MarketManager.cs
@@ -1,15 +1,382 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+public class MarketManager : MonoBehaviour
+{
+    public GameObject[] MarketObjs;
+    public Transform parent;
 
-public class MarketManager : MonoBehaviour {
+    public GameObject ShopList;
+    public GameObject PurchaseDialog;
+    public GameObject SellDialog;
+    public GameObject item;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject MoneyInfo;
+    Dictionary<string, Dictionary<string, string>> itemDic = new Dictionary<string, Dictionary<string, string>>();
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 20, 180, 30), "add 1000 Money"))
+        {
+            addMoney(1000);
+            PlayerDataManager.instance.AllItemCountInit();
+        };
+
+        if (GUI.Button(new Rect(300, 20, 180, 30), "xml"))
+        {
+            LoadXML();
+
+        };
+
+        if (GUI.Button(new Rect(500, 20, 180, 30), "init Item"))
+        {
+            PlayerDataManager.instance.AllItemCountInit();
+
+        };
+    }
+    // Use this for initialization
+    void Start()
+    {
+        initItemDic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void initItemDic()
+    {
+        XmlDocument xml = new XmlDocument();
+        XmlReaderSettings set = new XmlReaderSettings();
+        set.IgnoreComments = true;
+        xml.Load(XmlReader.Create((Application.dataPath + "/Items.xml"), set));
+
+        XmlNodeList xmlNodeList = xml.SelectSingleNode("objects").ChildNodes;
+        Debug.Log(xmlNodeList.Count);
+
+        foreach (XmlElement xl in xmlNodeList)
+        {
+
+            Debug.Log("id:" + xl.GetAttribute("id"));
+            Debug.Log("name:" + xl.GetAttribute("name"));
+            Debug.Log("price:" + xl.GetAttribute("price"));
+            string id = xl.GetAttribute("id");
+            string name = xl.GetAttribute("name");
+            string price = xl.GetAttribute("price");
+            Dictionary<string, string> tempDic = new Dictionary<string, string>();
+            tempDic.Add("name", name);
+            tempDic.Add("price", price);
+            itemDic.Add(id, tempDic);
+        }
+    }
+
+    public void Purchase()
+    {
+        //  CanvasManager.instance.ShowCanvas(GlobalDefine.GCanvas.BattleMenu);
+        for (int i = 0; i < MarketObjs.Length; i++)
+            MarketObjs[i].SetActive(false);
+
+        ShopList.SetActive(true);
+        MoneyInfo.SetActive(true);
+        int ownMoney = PlayerPrefs.GetInt("money", 0);
+        Text moneyContent = MoneyInfo.transform.FindChild("moneyContent").GetComponent<Text>();
+        moneyContent.text = ownMoney.ToString();
+
+
+        /*
+        XmlDocument xml = new XmlDocument();
+        XmlReaderSettings set = new XmlReaderSettings();
+        set.IgnoreComments = true;
+        xml.Load(XmlReader.Create((Application.dataPath + "/Items.xml"), set));
+
+        XmlNodeList xmlNodeList = xml.SelectSingleNode("objects").ChildNodes;
+        Debug.Log(xmlNodeList.Count);
+
+        foreach (XmlElement xl in xmlNodeList)
+        {
+
+            Debug.Log("id:" + xl.GetAttribute("id"));
+            Debug.Log("name:" + xl.GetAttribute("name"));
+            Debug.Log("price:" + xl.GetAttribute("price"));
+            string id = xl.GetAttribute("id");
+            string name = xl.GetAttribute("name");
+            string price = xl.GetAttribute("price");
+            Dictionary<string, string> tempDic = new Dictionary<string, string>();
+            tempDic.Add("name", name);
+            tempDic.Add("price", price);
+            itemDic.Add(id, tempDic);
+            */
+            for(int i=1;i<= itemDic.Count; i++) {
+
+            GameObject gobj = GameObject.Instantiate(item);
+            
+            gobj.name = i.ToString();// id
+            gobj.transform.SetParent(parent);
+            gobj.transform.localScale = parent.transform.localScale;
+            gobj.GetComponentInChildren<Text>().text = itemDic[i.ToString()]["name"];
+            Button btn = gobj.GetComponentInChildren<Button>();
+            btn.onClick.AddListener(delegate ()
+            {
+                this.OnBuyInformation(gobj);
+            });
+        }
+
+    }
+
+    public void Sell()
+    {
+        for (int i = 0; i < MarketObjs.Length; i++)
+            MarketObjs[i].SetActive(false);
+
+        ShopList.SetActive(true);
+        MoneyInfo.SetActive(true);
+        int ownMoney = PlayerPrefs.GetInt("money", 0);
+        Text moneyContent = MoneyInfo.transform.FindChild("moneyContent").GetComponent<Text>();
+        moneyContent.text = ownMoney.ToString();
+        //
+        Dictionary<string, int> ownItemDic = PlayerDataManager.instance.GetAllItemCount();
+
+
+        for (int i = 1; i <= ownItemDic.Count; i++)
+        {
+            // ownItemDic[i]
+
+
+            int q = ownItemDic[i.ToString()];
+
+            Debug.Log("i:" + i + "  Count:" + q);
+            if (q == 0)
+                continue;
+            GameObject gobj = GameObject.Instantiate(item);
+
+            gobj.name = i.ToString();// id
+            gobj.transform.SetParent(parent);
+            gobj.transform.localScale = parent.transform.localScale;
+
+
+            string name = itemDic[i.ToString()]["name"];
+            Debug.Log("name:" + name);
+            gobj.GetComponentInChildren<Text>().text = name;
+
+            //  ownItemDic[i].Keys name
+            Button btn = gobj.GetComponentInChildren<Button>();
+            btn.onClick.AddListener(delegate ()
+            {
+                this.OnSellInformation(gobj);
+            });
+        }
+
+
+        /*
+        foreach (KeyValuePair<string, int> item in ownItemDic)
+        {
+            GameObject gobj = GameObject.Instantiate(item);
+          
+            gobj.name = xl.GetAttribute("id");// id
+            gobj.transform.SetParent(parent);
+            gobj.transform.localScale = parent.transform.localScale;
+            gobj.GetComponentInChildren<Text>().text = xl.GetAttribute("name"); ;
+            Button btn = gobj.GetComponentInChildren<Button>();
+            btn.onClick.AddListener(delegate ()
+            {
+                this.OnEmailInformation(gobj);
+            });
+        }*/
+
+    }
+
+    public void OnBuyInformation(GameObject sender)
+    {
+        Debug.Log("Test:" + sender.name);
+
+        PurchaseDialog.SetActive(true);
+        Dictionary<string, string> tempDic = itemDic[sender.name];
+
+        PurchaseDialog.transform.FindChild("title").GetComponent<Text>().text = tempDic["name"];
+        PurchaseDialog.transform.FindChild("price").GetComponent<Text>().text = tempDic["price"];
+
+        PurchaseDialog.transform.FindChild("ID").GetChild(0).gameObject.name = sender.name;
+        //Debug.Log("@@:" + tempDic["name"]);
+        int count = PlayerDataManager.instance.GetItemCount(sender.name);
+        PurchaseDialog.transform.FindChild("holdNum").GetComponent<Text>().text = count.ToString();
+    }
+
+    public void OnSellInformation(GameObject sender)
+    {
+        Debug.Log("Test:" + sender.name);
+
+        SellDialog.SetActive(true);
+        Dictionary<string, string> tempDic = itemDic[sender.name];
+
+        SellDialog.transform.FindChild("title").GetComponent<Text>().text = tempDic["name"];
+       int price= int.Parse(tempDic["price"])/2;
+
+        SellDialog.transform.FindChild("price").GetComponent<Text>().text = price.ToString();
+
+        SellDialog.transform.FindChild("ID").GetChild(0).gameObject.name = sender.name;
+        //Debug.Log("@@:" + tempDic["name"]);
+        int count = PlayerDataManager.instance.GetItemCount(sender.name);
+        SellDialog.transform.FindChild("holdNum").GetComponent<Text>().text = count.ToString();
+    }
+
+
+    public void Buy(int num)
+    {
+        //品項
+        string title = PurchaseDialog.transform.FindChild("title").GetComponent<Text>().text;
+        // string title = PurchaseDialog.transform.FindChild("title").GetComponent<Text>().text;
+        //價格
+        int price = int.Parse(PurchaseDialog.transform.FindChild("price").GetComponent<Text>().text);
+
+        Debug.Log("price:" + price);
+        //目前數量+num
+        string id = PurchaseDialog.transform.FindChild("ID").GetChild(0).gameObject.name;
+        int count = PlayerDataManager.instance.GetItemCount(id);
+
+
+
+        bool moneyEnough = reduceMoney(price * num);
+        if (!moneyEnough)
+            return;
+        Text hold = PurchaseDialog.transform.FindChild("holdNum").GetComponent<Text>();
+        count += num;
+        hold.text = count.ToString();
+        PlayerDataManager.instance.SetItemCount(id, count);
+
+    }
+
+
+    public void Sell(int num)
+    {
+        //品項
+        string title = SellDialog.transform.FindChild("title").GetComponent<Text>().text;
+        // string title = PurchaseDialog.transform.FindChild("title").GetComponent<Text>().text;
+        //價格
+        int price = int.Parse(SellDialog.transform.FindChild("price").GetComponent<Text>().text);
+
+        Debug.Log("price:" + price);
+        //目前數量+num
+        string id = SellDialog.transform.FindChild("ID").GetChild(0).gameObject.name;
+        int count = PlayerDataManager.instance.GetItemCount(id);
+
+
+        ////
+        if (num > count)
+        {
+            Debug.Log("沒那多物品:" + count);
+            return;
+        }
+
+        Text hold = SellDialog.transform.FindChild("holdNum").GetComponent<Text>();
+        count-= num;
+        Debug.Log("剩餘數量:" + count);
+        hold.text = count.ToString();
+        PlayerDataManager.instance.SetItemCount(id, count);
+        //家前
+        addMoney(price*num);
+    }
+
+
+    public void ClosePurchaseDialog()
+    {
+        PurchaseDialog.SetActive(false);
+    }
+
+    public void CloseSellDialog()
+    {
+        SellDialog.SetActive(false);
+    }
+
+
+    private void addMoney(int price)
+    {
+        int ownMoney = PlayerPrefs.GetInt("money", 0);
+        ownMoney += price;
+
+        PlayerPrefs.SetInt("money", ownMoney);
+        PlayerPrefs.Save();
+
+        Text moneyContent = MoneyInfo.transform.FindChild("moneyContent").GetComponent<Text>();
+        moneyContent.text = ownMoney.ToString();
+    }
+
+    private bool reduceMoney(int price)
+    {
+        Debug.Log("reduceMoney:" + price);
+        int ownMoney = PlayerPrefs.GetInt("money", 0);
+
+        if (price > ownMoney)
+        {
+            Debug.Log("餘額不足");
+            return false;
+        }
+
+        ownMoney -= price;
+
+        PlayerPrefs.SetInt("money", ownMoney);
+        PlayerPrefs.Save();
+
+        Text moneyContent = MoneyInfo.transform.FindChild("moneyContent").GetComponent<Text>();
+        moneyContent.text = ownMoney.ToString();
+        return true;
+    }
+
+    void CreateXML()
+    {
+        string path = Application.dataPath + "/Items.xml";
+        if (!File.Exists(path))
+        {
+
+            XmlDocument xml = new XmlDocument();
+            XmlElement root = xml.CreateElement("objects");
+
+            XmlElement main = xml.CreateElement("items");
+            main.SetAttribute("id", "1");
+            main.SetAttribute("name", "初級藥水");
+            main.SetAttribute("price", "100");
+            root.AppendChild(main);
+
+            XmlElement main2 = xml.CreateElement("items");
+            main2.SetAttribute("id", "2");
+            main2.SetAttribute("name", "中級藥水");
+            main2.SetAttribute("price", "300");
+            root.AppendChild(main2);
+
+
+
+            xml.AppendChild(root);
+
+            xml.Save(path);
+        }
+    }
+
+    void LoadXML()
+    {
+        XmlDocument xml = new XmlDocument();
+        XmlReaderSettings set = new XmlReaderSettings();
+        set.IgnoreComments = true;
+        xml.Load(XmlReader.Create((Application.dataPath + "/Items.xml"), set));
+
+        XmlNodeList xmlNodeList = xml.SelectSingleNode("objects").ChildNodes;
+        foreach (XmlElement xl1 in xmlNodeList)
+        {
+
+            Debug.Log("id:" + xl1.GetAttribute("id"));
+            Debug.Log("name:" + xl1.GetAttribute("name"));
+            Debug.Log("price:" + xl1.GetAttribute("price"));
+        }
+
+    }
+
+
+
+
+
+
 }
--- Assets/Scripts/PlayerDataManager.cs
+++ Assets/Scripts/PlayerDataManager.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class PlayerDataManager {
 
 	private static PlayerDataManager _instance;
-	public static PlayerDataManager instance {
+    Dictionary<string, int> ownItemDic = new Dictionary<string, int>();
+    public static PlayerDataManager instance {
 		get
 		{
 			if (_instance == null)
@@ -17,4 +18,103 @@ public class PlayerDataManager {
 	{
 		
 	}
+
+    public void initOwnItems()
+    {
+        //刪除 PlayerPrefs 中某一個key的值
+        //  PlayerPrefs.DeleteKey(“key”);
+
+        //判斷 PlayerPrefs中是否存在這個key
+        // bool b = PlayerPrefs.HasKey(“key”);
+
+      //  PlayerPrefs.GetString("testStr", "default");
+       // PlayerPrefs.SetString("testStr", "default");
+    }
+    /*
+    public int GetItemData(string item)
+    {
+      return  PlayerPrefs.GetInt(item, 0);
+   
+    }
+    public void SetItemData(string item,int count)
+    {
+        PlayerPrefs.SetInt(item, count);
+        PlayerPrefs.Save();
+    }
+    */
+
+    public int GetItemCount(string item)
+    {
+        return PlayerPrefs.GetInt(item, 0);
+
+    }
+    public void SetItemCount(string item, int count)
+    {
+        PlayerPrefs.SetInt(item, count);
+        ownItemDic[item] = count;
+        PlayerPrefs.Save();
+    }
+
+
+    public void AllItemCountInit()
+    {
+        PlayerPrefs.SetInt("1", 0);
+        PlayerPrefs.SetInt("2", 0);
+        PlayerPrefs.SetInt("3", 0);
+        PlayerPrefs.SetInt("4", 0);
+        PlayerPrefs.SetInt("5", 0);
+        PlayerPrefs.SetInt("6", 0);
+        PlayerPrefs.SetInt("7", 0);
+        PlayerPrefs.SetInt("8", 0);
+        PlayerPrefs.SetInt("9", 0);
+        PlayerPrefs.SetInt("10", 0);
+        PlayerPrefs.SetInt("11", 0);
+        PlayerPrefs.SetInt("12", 0);
+        PlayerPrefs.SetInt("13", 0);
+        PlayerPrefs.SetInt("14", 0);
+        PlayerPrefs.SetInt("15", 0);
+        PlayerPrefs.SetInt("16", 0);
+        PlayerPrefs.Save();
+    }
+
+    public Dictionary<string, int> GetAllItemCount()
+    {
+  
+       
+      
+
+        ownItemDic.Add("1", PlayerPrefs.GetInt("1", 0));
+
+        ownItemDic.Add("2", PlayerPrefs.GetInt("2", 0));
+    
+        ownItemDic.Add("3", PlayerPrefs.GetInt("3", 0));
+      
+        ownItemDic.Add("4", PlayerPrefs.GetInt("4", 0));
+        
+        ownItemDic.Add("5", PlayerPrefs.GetInt("5", 0));
+      
+        ownItemDic.Add("6", PlayerPrefs.GetInt("6", 0));
+      
+        ownItemDic.Add("7", PlayerPrefs.GetInt("7", 0));
+     
+        ownItemDic.Add("8", PlayerPrefs.GetInt("8", 0));
+      
+        ownItemDic.Add("9", PlayerPrefs.GetInt("9", 0));
+  
+        ownItemDic.Add("10", PlayerPrefs.GetInt("10", 0));
+     
+        ownItemDic.Add("11", PlayerPrefs.GetInt("11", 0));
+  
+        ownItemDic.Add("12", PlayerPrefs.GetInt("12", 0));
+    
+        ownItemDic.Add("13", PlayerPrefs.GetInt("13", 0));
+     
+        ownItemDic.Add("14", PlayerPrefs.GetInt("14", 0));
+      
+        ownItemDic.Add("15", PlayerPrefs.GetInt("15", 0));
+     
+        ownItemDic.Add("16", PlayerPrefs.GetInt("16", 0));
+ 
+        return ownItemDic;
+    }
 }
